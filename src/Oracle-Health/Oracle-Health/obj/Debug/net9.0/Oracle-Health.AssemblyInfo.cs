4c91f8d5a91aa0011dc42f635bb58350f15cea95
MohamedJaafar0 <202304048@student.polytechnic.bh>
2026-05-20 15:45:44 +0300

    removed Booking
    Only the receptionist can book now

--- src/Oracle-Health/Oracle-Health/obj/Debug/net9.0/Oracle-Health.AssemblyInfo.cs
+++ src/Oracle-Health/Oracle-Health/obj/Debug/net9.0/Oracle-Health.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oracle-Health")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791c5137139ff4bdd0bbfd0e3ee064ce884ab7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b2f22e0be3e59070bc3b68bb0e13c73e0adbcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oracle-Health")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oracle-Health")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
